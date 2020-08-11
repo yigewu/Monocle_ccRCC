7d88093e9315f0e405fd0054babd8b7d07d488a2
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2020-08-11 09:38:02 -0500

    pooled normal v1

---
+++ C3N-01200_PooledNormalEpiCells_ByCellType/0.create_monocle_obj.sh
@@ -0,0 +1,9 @@
+# use cell type for differntial expression analysis; q cut-off set as 1e-10
+id_run="C3N-01200_PooledNormalEpiCells_ByCellType"
+path_out="/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/Resources/snRNA_Processed_Data/Monocle/outputs/"${id_run}"/"
+mkdir -p ${path_out}
+Rscript ./pseudotime_analysis.R \
+--FormulaStr=Cell_type.detailed \
+--q_val=1e-10 \
+--out_path=${path_out} \
+--force.reprocess=TRUE &> ${path_out}Log.$(date +%Y%m%d%H%M%S).txt&
---
+++ C3N-01200_PooledNormalEpiCells_ByCellType/3.visualize_with_updated_monocle_obj.sh
@@ -0,0 +1,6 @@
+# use sampel id for differntial expression analysis; q cut-off set as 1e-10
+id_run="C3N-01200_PooledNormalEpiCells_ByCellType"
+path_out="/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/Resources/snRNA_Processed_Data/Monocle/outputs/"${id_run}"/"
+Rscript ./visualize_cell_distribution.R \
+--out_path=${path_out} \
+--monocle_obj=${path_out}combined_subset_pseudotime_qval_1e-10.rds &> ${path_out}Log.$(date +%Y%m%d%H%M%S).txt&
