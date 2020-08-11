864d141a48d0b64e8f3091ab4d021f62fe9ae1d9
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2020-08-11 10:54:47 -0500

    pooled normal + C3L-00088 v1

---
+++ C3L-00088_PooledNormalEpiCells_ByCellType/project_config.sh
@@ -0,0 +1,3 @@
+## shared parameters
+id_run="C3L-00088_PooledNormalEpiCells_ByCellType"
+path_out="/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/Resources/snRNA_Processed_Data/Monocle/outputs/"${id_run}"/"
